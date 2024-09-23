0d11f4a221dfdd052924f3ececacb5836b4ac586
aneneme <148064049+aneneme@users.noreply.github.com>
2024-09-23 09:26:34 -0300

    .

--- script.js
+++ script.js
@@ -9,67 +9,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
 
-const perguntas = [
-    {
-        enunciado: "Qual dos animais é um mamífero? ",
-        alternativas: [
-            {
-                texto: "Baleia ",
-                afirmacao: [
-                    "Se essa foi a opção escolhida, está correta",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto: "Cavalo marinho",
-                afirmacao: [
-                    "Se essa foi a opção, está correta",
-                    "afirmacao 2"
-                    ]
-            }           
-            
-        ]
-    },
-    {
-        enunciado: "Qual o unico mamifero que voa?",
-        alternativas: [
-            {
-                texto:"Alternativa 1 da pergunta 2",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto: "Alternativa 2 da pergunta 2",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
-        ]
-    },
-    {
-        enunciado: "Qual o mamifero que bota ovo?",
-        alternativas: [
-            {
-                texto:"Alternativa 1 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            },
-            {
-                texto:"Alternativa 2 da pergunta 3",
-                afirmacao: [
-                    "afirmacao 1",
-                    "afirmacao 2"
-                    ]
-            }
-            
-        ]
-    },
-];
+
 
 let atual = 0; 
 let perguntaAtual;
